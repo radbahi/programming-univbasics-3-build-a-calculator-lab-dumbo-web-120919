813e8d3168a818aab2b4de16ed4aebaa88a62f36
Radouane Bahi <radbahi@gmail.com>
2019-11-14 03:41:41 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+result = num1 + num2
+result
 end
 
 def subtraction(num1, num2)
-
+result = num1 - num2
+result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  result
 end
 
 def modulo(num1, num2)
@@ -19,5 +23,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+result = num**2 
+result
 end
